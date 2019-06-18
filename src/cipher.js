3e408dd5b6c30a6f705ebd59c4c693f3879574cd
AdrianaRosas <51386430+AdrianaRosas@users.noreply.github.com>
2019-06-18 22:39:12 +0200

    Subiendo cambios en cipher y demás elementos

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,48 @@
-window.cipher 
-//Cifrado
-function encode () {
-	const m = document.getElementById("texto").value; //Se le da valor al texto introducido
-	let desplazar = document.getElementById("shift").value; //Lugares a desplazar
-	let s = parseInt(desplazar);
-	let result = " "; //variable para la fórmula
-	for (let i = 0; i < m.length; i++){ 
-		let letter = m.charCodeAt(i); //devolviendo el caracter y convirtiendo el texto a codigo ASCII
-	  if (65 <= letter && letter <= 90) result += String.fromCharCode((letter - 65 + s) % 26 + 65);// Aplicando formula de conversion mayúsculas por su posición en el código ASCII
-	  else if (97 <= letter && letter <= 122) result += String.fromCharCode((letter - 97 + s) % 26 + 97); // Aplicando forumula de conversion minúsculas por su posición en el cógigo ASCII
-	  else                          result += m.charAt(i) // Devuelve el primer caracter de la nueva cadena
-	
-	  document.getElementById("mostrarResultado").value = 'Tu nuevo código es:' + result; // Resultado en HTML
+window.cipher = {
+  //Cifrado
+  encode: (offset, str) => {
+    offset = parseInt(offset);
+    let result = ""; //variable para la fórmula
+    for (let i = 0; i < str.length; i++) {
+      let letter = str.charCodeAt(i); //devolviendo el caracter y convirtiendo el texto a codigo ASCII
+      if (letter >= 65 && letter <= 90) result += String.fromCharCode((letter - 65 + offset) % 26 + 65);
+      // Aplicando formula de conversion mayúsculas por su posición en el código ASCII
+      else if (97 <= letter && letter <= 122) result += String.fromCharCode((letter - 97 + offset) % 26 + 97); // Aplicando forumula de conversion minúsculas por su posición en el cógigo ASCII
+      else result += str.charAt(i) // Devuelve el primer caracter de la nueva cadena
 	}
-  }
-  
+    return result
+  },
+
   // Descifrado//
-  function decode () { 
-	const m = document.getElementById("texto").value;//Se le da valor al texto introducido
-	let desplazar = document.getElementById("shift").value; //Lugares a desplazar
-	  let s = parseInt(desplazar);
-	  let result = " "; //variable para la fórmula
-	for (let i = 0; i < m.length; i++){
-		let letter = m.charCodeAt(i);//convirtiendo el texto a codigo ASCII
-		if (65 <= letter && letter <=(90-s) ) result += String.fromCharCode((letter - 65 - s) % 26 + 65);// Aplicando la fórmula para letras mayúsculas
-		else if (((90-s)+1) <= letter && letter <= (90-s)) result += String.fromCharCode((letter - 97 - s) % 26 + 97);// Aplicando la fórmula para letras minúsculas
-	  else                          result += m.charAt(i) // Devuelve el primer caracter de la nueva cadena
-	  document.getElementById("mostrarResultado").value = result;// Resultado en HTML
-	
-	}
+  decode: (offset, str) => {
+    offset = parseInt(offset);
+    let result = ""; //variable para la fórmula
+    for (let i = 0; i < str.length; i++) {
+      let letter = str.charCodeAt(i); //convirtiendo el texto a codigo ASCII
+      if (letter >= 65 && letter <= 90 - offset) result += String.fromCharCode((letter - 65 - offset) % 26 + 65); // Aplicando la fórmula para letras mayúsculas
+      else if (((90 - offset) + 1) <= letter && letter <= (90 - offset)) result += String.fromCharCode((letter - 97 - offset) % 26 + 97); // Aplicando la fórmula para letras minúsculas
+      else result += str.charAt(i) // Devuelve el primer caracter de la nueva cadena
+    }
+    return result
+  },
+
+
+
+
+
+   //decode: (offset, str) => {
+     //const m = document.getElementById("texto").value; //Se le da valor al texto introducido
+     //let desplazar = document.getElementById("shift").value; //Lugares a desplazar
+     //let s = parseInt(desplazar);
+     //let result = " "; //variable para la fórmula
+     //for (let i = 0; i < m.length; i++) {
+      // let letter = m.charCodeAt(i); //convirtiendo el texto a codigo ASCII
+      // if (65 <= letter && letter <= (90 - s)) result += String.fromCharCode((letter - 65 - s) % 26 + 65); // Aplicando la fórmula para letras mayúsculas
+      // else if (((90 - s) + 1) <= letter && letter <= (90 - s)) result += String.fromCharCode((letter - 97 - s) % 26 + 97); // Aplicando la fórmula para letras minúsculas
+      // else result += m.charAt(i) // Devuelve el primer caracter de la nueva cadena
+     // document.getElementById("mostrarResultado").value = result; // Resultado en HTML
+
+    }
+
   
-  }
+
--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
+const datos = document.getElementById('datos')
+const m = document.getElementById("texto"); //Se le da valor al texto introducido
+let desplazar = document.getElementById("shift"); //Lugares a desplazar
+let empezar= document.getElementById("empezar");
+empezar.addEventListener("click", mostrar)
 
-function mostrar(id){
-    if (document.getElementById){ //se obtiene el id
-    var el = document.getElementById(id); //buscando el id
+function mostrar() {
+  if (document.getElementById) { //se obtiene el id
+    var el = document.getElementById('contenido'); //buscando el id
     el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
-    }
-    }
-    window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
-    mostrar('contenido');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
-    }
+  }
+}
+window.onload = function () {
+  /*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+  mostrar('contenido'); /* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+}
 
-    function enviarTexto()
-    {
-        let persona=document.getElementById("usuario").value;
-        let lugar=document.getElementById("departamento").value;
-        document.getElementById("resultado").value='¡Hola'+ " " + persona + " " + "del" + " " + lugar +"!";
-    };  
+function enviarTexto() {
+  let persona = document.getElementById("usuario").value;
+  let lugar = document.getElementById("departamento").value;
+  document.getElementById("resultado").value = '¡Hola' + " " + persona + " " + "del" + " " + lugar + "!";
+}
 
 //variables de los botones
-let cifrar = document.getElementById("cifrado"); 
+let cifrar = document.getElementById("cifrado");
 let descifrar = document.getElementById("descifrado");
 
 
 //Eventos a los botones
-cifrar.addEventListener("click", encode);
-descifrar.addEventListener("click", decode);
+cifrar.addEventListener("click", () => {
+    let str = m.value
+    let offset = desplazar.value
+    const resp = window.cipher.encode(offset, str)
+	document.getElementById("mostrarResultado").value = 'Tu nuevo código es:' + resp; // Resultado en HTML
+
+});
+
+descifrar.addEventListener("click", () => {
+  let str = m.value
+  let offset = desplazar.value
+  const resp = window.cipher.decode(offset, str)
+document.getElementById("mostrarResultado").value = 'Tu nuevo código es:' + resp; // Resultado en HTML
+
+});
+
+
+// descifrar.addEventListener("click", decode);
+datos.addEventListener('click', enviarTexto)
+
 
