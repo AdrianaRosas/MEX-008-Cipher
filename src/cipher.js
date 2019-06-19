3aed6b88a37c1dca36a5671bcb9b09846b1eff66
AdrianaRosas <51386430+AdrianaRosas@users.noreply.github.com>
2019-06-20 00:55:03 +0200

    Subiendo cambios finales

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,12 @@
 window.cipher = {
   //Cifrado
-  encode: (offset, str) => {
-    offset = parseInt(offset);
+  encode: (offset, str) => { //función de codificado que recibirá el valor del string y el offset
+    offset = parseInt(offset); //busca un número en el input
     let result = ""; //variable para la fórmula
-    for (let i = 0; i < str.length; i++) {
-      let letter = str.charCodeAt(i); //devolviendo el caracter y convirtiendo el texto a codigo ASCII
-      if (letter >= 65 && letter <= 90) result += String.fromCharCode((letter - 65 + offset) % 26 + 65);
-      // Aplicando formula de conversion mayúsculas por su posición en el código ASCII
+    for (let i = 0; i < str.length; i++) { //se crea un bucle  con una variable interna, luego mide el tamaño del string y lo recorre 
+      let letter = str.charCodeAt(i); //variable para devolver el caracter y convirtiendo el texto al valor asignado en el if y else
+      if (letter >= 65 && letter <= 90) result += String.fromCharCode((letter - 65 + offset) % 26 + 65); //se usa el operador && para que se cumplan las condiciones de mayor que y menor por sus posiciones en el código ASCII
+      //devuelve el caracter de letter después de aplicar la fórmula + el valor del offset
       else if (97 <= letter && letter <= 122) result += String.fromCharCode((letter - 97 + offset) % 26 + 97); // Aplicando forumula de conversion minúsculas por su posición en el cógigo ASCII
       else result += str.charAt(i) // Devuelve el primer caracter de la nueva cadena
 	}
@@ -14,13 +14,13 @@ window.cipher = {
   },
 
   // Descifrado//
-  decode: (offset, str) => {
+  decode: (offset, str) => { //función de decodificado
     offset = parseInt(offset);
     let result = ""; //variable para la fórmula
     for (let i = 0; i < str.length; i++) {
-      let letter = str.charCodeAt(i); //convirtiendo el texto a codigo ASCII
-      if (letter >= 65 && letter <= 90 - offset) result += String.fromCharCode((letter - 65 - offset) % 26 + 65); // Aplicando la fórmula para letras mayúsculas
-      else if (((90 - offset) + 1) <= letter && letter <= (90 - offset)) result += String.fromCharCode((letter - 97 - offset) % 26 + 97); // Aplicando la fórmula para letras minúsculas
+      let letter = str.charCodeAt(i); 
+      if (letter >= 65 && letter <= 90 - offset) result += String.fromCharCode((letter - 65 - offset) % 26 + 65); //se usa el operador && para que se cumplan las condiciones de mayor que y menor por sus posiciones en el código ASCII
+      else if (((90 - offset) + 1) <= letter && letter <= (90 - offset)) result += String.fromCharCode((letter - 97 - offset) % 26 + 97); 
       else result += str.charAt(i) // Devuelve el primer caracter de la nueva cadena
     }
     return result
@@ -42,7 +42,36 @@ window.cipher = {
       // else result += m.charAt(i) // Devuelve el primer caracter de la nueva cadena
      // document.getElementById("mostrarResultado").value = result; // Resultado en HTML
 
-    }
+    
+  //Cifrado
+  //encode: (offset, str) => { //función de codificado
+    //offset = parseInt(offset); //busca un número en el input 
+    //let result = ""; //variable para la fórmula
+    //for (let i = 0; i < str.length; i++) { //se crea un bucle  con una variable interna, luego mide el tamaño del string y lo recorre 
+     // let letter = str.charCodeAt(i); //variable para devolver el caracter y convirtiendo el texto al valor asignado en el if y else
+     // if (letter >= 65 && letter <= 90) result += String.fromCharCode((letter - 65 + offset) % 26 + 65); //se usa el operador && para que se cumplan las condiciones de mayor que y menor por sus posiciones en el código ASCII
+      //devuelve el caracter de letter después de aplicar la fórmula + el valor del offset
+     // else if (65 <= letter && letter <= 122) result += String.fromCharCode((letter - 65 + offset) % 26 + 90); //la otra condición es para las mayúsculas y los cararcteres especiales por su posición en el código ASCII
+      //else result += str.charAt(i) // Devuelve el primer caracter de la nueva cadena después de aplicar la fórmula
+//	}
+   // return result
+ // },
+
+
+
+   //decode: (offset, str) => {
+     //const m = document.getElementById("texto").value; //Se le da valor al texto introducido
+     //let desplazar = document.getElementById("shift").value; //Lugares a desplazar
+     //let s = parseInt(desplazar);
+     //let result = " "; //variable para la fórmula
+     //for (let i = 0; i < m.length; i++) {
+      // let letter = m.charCodeAt(i); //convirtiendo el texto a codigo ASCII
+      // if (65 <= letter && letter <= (90 - s)) result += String.fromCharCode((letter - 65 - s) % 26 + 65); // Aplicando la fórmula para letras mayúsculas
+      // else if (((90 - s) + 1) <= letter && letter <= (90 - s)) result += String.fromCharCode((letter - 97 - s) % 26 + 97); // Aplicando la fórmula para letras minúsculas
+      // else result += m.charAt(i) // Devuelve el primer caracter de la nueva cadena
+     // document.getElementById("mostrarResultado").value = result; // Resultado en HTML
+
+   // }
 
   
-
+}
--- src/index.js
+++ src/index.js
@@ -1,50 +1,52 @@
-const datos = document.getElementById('datos')
-const m = document.getElementById("texto"); //Se le da valor al texto introducido
-let desplazar = document.getElementById("shift"); //Lugares a desplazar
-let empezar= document.getElementById("empezar");
-empezar.addEventListener("click", mostrar)
+const datos = document.getElementById('datos') //asignamos variable para el botón de datos del usuario
+const txt = document.getElementById("texto"); //variable para el texto introducido 
+let desplazar = document.getElementById("shift"); //variable para offset
+let empezar= document.getElementById("empezar"); //variable para el botón "empezar", que despliega la siguente sección
+empezar.addEventListener("click", mostrar) //evento del click para mostrar la sección oculta en la página
 
-function mostrar() {
-  if (document.getElementById) { //se obtiene el id
-    var el = document.getElementById('contenido'); //buscando el id
-    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+
+function mostrar() { //función para desplegar la segunda sección del documento
+  if (document.getElementById) { 
+    var cont = document.getElementById('contenido'); //se crea una variable para ubicar el div a desplegar y se ubica en el HTML
+    cont.style.display = (cont.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div con una función ternaria
+   
   }
 }
 window.onload = function () {
-  /*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
-  mostrar('contenido'); /* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+  //hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente//
+  mostrar('contenido'); //"contenido" es el nombre del DIV
 }
 
-function enviarTexto() {
-  let persona = document.getElementById("usuario").value;
-  let lugar = document.getElementById("departamento").value;
-  document.getElementById("resultado").value = '¡Hola' + " " + persona + " " + "del" + " " + lugar + "!";
+function enviarTexto() { //función para mostrar el resultado del nombre y el departamento 
+  let persona = document.getElementById("usuario").value; //variable para darle valor al input del nombre 
+  let lugar = document.getElementById("departamento").value; //variable para darle valor al input del departamento
+  document.getElementById("resultado").value = '¡Hola' + " " + persona + " " + "del" + " " + lugar + "!"; //resultado  
 }
+datos.addEventListener('click', enviarTexto) //evento del click para que aparezcan los datos del ususario
 
 //variables de los botones
-let cifrar = document.getElementById("cifrado");
-let descifrar = document.getElementById("descifrado");
+let cifrar = document.getElementById("cifrado"); // variable para el botón de cifrado
+let descifrar = document.getElementById("descifrado"); //variable para el botón de descifrado
 
 
 //Eventos a los botones
-cifrar.addEventListener("click", () => {
-    let str = m.value
-    let offset = desplazar.value
-    const resp = window.cipher.encode(offset, str)
-	document.getElementById("mostrarResultado").value = 'Tu nuevo código es:' + resp; // Resultado en HTML
+cifrar.addEventListener("click", () => { //función para el botón de cifrado
+    let str = txt.value //variable para darle valor al texto a codificar 
+    let offset = desplazar.value //valor para el offset
+    const resp = window.cipher.encode(offset, str) //variable que va a buscar la función de codificar en el archivo cipher junto con sus valores(offset y str)
+	document.getElementById("mostrarResultado").value = 'Tu nuevo código es:' + resp; // Resultado en HTML en el input correspondiente
 
 });
 
-descifrar.addEventListener("click", () => {
-  let str = m.value
+descifrar.addEventListener("click", () => { //función para el botón de descifrado
+  let str = txt.value 
   let offset = desplazar.value
-  const resp = window.cipher.decode(offset, str)
-document.getElementById("mostrarResultado").value = 'Tu nuevo código es:' + resp; // Resultado en HTML
+  const resp = window.cipher.decode(offset, str) //variable que va a buscar la función de decodificar en el archivo cipher
+document.getElementById("mostrarResultado").value = 'Tu nuevo código es:' + resp; // Resultado en HTML en el input correspondiente
 
 });
 
 
-// descifrar.addEventListener("click", decode);
-datos.addEventListener('click', enviarTexto)
+
 
 
